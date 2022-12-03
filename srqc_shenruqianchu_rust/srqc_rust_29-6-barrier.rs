9e3b541b6fe92e5cf4ebcd016e08667caa220f01
Laris Qiao <laris.qiao@gmail.com>
2022-12-03 22:20:19 +0800

    update srqc_rust_29-6-barrier.rs

--- srqc_shenruqianchu_rust/srqc_rust_29-6-barrier.rs
+++ srqc_shenruqianchu_rust/srqc_rust_29-6-barrier.rs
@@ -2,21 +2,22 @@ use std::sync::{Arc, Barrier};
 use std::thread;
 
 fn main() {
-	let barrier = Arc::new(Barrier::new(10));
-	let mut handlers = vec![];
-	for idx_thread in 0..10 {
-		let c = barrier.clone();
-		// THe same messages will be printed together.
-		// You will NOT see any interleaving.
-		let t = thread::spawn(move || {
-			println!("Thread {} before wait", idx_thread);
-			c.wait();
-			println!("Thread {} after wait", idx_thread);
-		});
-		handlers.push(t);
-	}
+  println!("Main fn Thread ID: {:2?}, {:?}", thread::current().id(), thread::current().name());
+  let barrier = Arc::new(Barrier::new(10));
+  let mut handlers = vec![];
+  for idx_thread in 0..10 {
+    let c = barrier.clone();
+    // THe same messages will be printed together.
+    // You will NOT see any interleaving.
+    let t = thread::spawn(move || {
+      println!("Before: Thread ID: {:2?} with idx_thread: {idx_thread}, Thread name: {:?}", thread::current().id(), thread::current().name());
+      c.wait();
+      println!("After:  Thread ID: {:2?} with idx_thread: {idx_thread}, Thread name: {:?}", thread::current().id(), thread::current().name());
+    });
+    handlers.push(t);
+  }
 
-	for h in handlers {
-		h.join().ok();
-	}
+  for h in handlers {
+    h.join().ok();
+  }
 }
