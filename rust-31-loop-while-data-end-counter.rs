a69dd23ca12e225a99ec2274628cc2a38db63538
Laris Qiao <laris.qiao@gmail.com>
2023-09-21 23:06:12 +0800

    update loop while model

--- rust-31-loop-while-data-end-counter.rs
+++ rust-31-loop-while-data-end-counter.rs
@@ -1,4 +1,78 @@
 fn main() {
+// rust - loop while model
+// do: do work
+// break: condition to break loop
+// cnt: counter
+// start/end: init cnt and stop cnt
+// n: loop iteration times
+// while = loop { break - do/cnt }
+//----------------------------------------
+// 1: do - break - cnt - start - end - n
+let start = 1;
+let end   = 3;
+let mut cnt = start;
+loop {
+    // do
+    if cnt == end { break; }
+    cnt += 1;
+}
+//----------------------------------------
+// 2: do - cnt - break - start - end+1 - n
+let start = 1;
+let end   = 3;
+let mut cnt = start;
+loop {
+    // do
+    cnt += 1;
+    if cnt == end+1 { break; }
+}
+//----------------------------------------
+// 3: break - do - cnt - start - end+1 - n+1
+let start = 1;
+let end   = 3;
+let mut cnt = start;
+loop {
+    if cnt == end+1 { break; }
+    // do
+    cnt += 1;
+}
+//----------------------------------------
+// 4: break - cnt - do - start - end+1 - n+1
+let start = 1;
+let end   = 3;
+let mut cnt = start;
+loop {
+    if cnt == end+1 { break; }
+    cnt += 1;
+    // do
+}
+//----------------------------------------
+// 5: cnt - break - do - start - end+2 - n+1
+let start = 1;
+let end   = 3;
+let mut cnt = start;
+loop {
+    cnt += 1;
+    if cnt == end+2 { break; }
+    // do
+}
+//----------------------------------------
+// 6: cnt - do - break - start - end+1 - n
+let start = 1;
+let end   = 3;
+let mut cnt = start;
+loop {
+    cnt += 1;
+    // do
+    if cnt == end+1 { break; }
+}
+//----------------------------------------
+// while = loop { break - do/cnt } end n
+// while pre-check condition before loop
+// while cnt <= end {
+  // do or cnt;
+// }
+//----------------------------------------
     println!("1-3-use-end-cnt");
     let mut i = 1;
     let mut loop_cnt = 0;
@@ -99,7 +173,7 @@ use i: 3
 loop cnt: 4, loop_in: 3:w
 */
 
-    println!("while-1-3-end-use-counter-one-more-iter");
+    println!("while-1-3-end-use-counter");
     let mut i = 1;
     let mut loop_cnt = 0;
     while i <= 3 {
@@ -117,7 +191,7 @@ use i: 2
 loop cnt: 3, loop_in: 3
 use i: 3
 */
-    println!("while-0-2-end-counter-use-one-more-iter");
+    println!("while-0-2-end-counter-use");
     let mut i = 0;
     let mut loop_cnt = 0;
     while i <= 2 {
