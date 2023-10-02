622778d66537435761efc3d06f432288ca70a3ed
Laris Qiao <laris.qiao@gmail.com>
2023-10-02 08:48:56 +0800

    add a13 vector, borrow with &

--- rust-course-data/exercises/activities/src/bin/a13-6-5-vectors-for-loop-activity.rs
+++ rust-course-data/exercises/activities/src/bin/a13-6-5-vectors-for-loop-activity.rs
@@ -10,4 +10,19 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // * Use a vector to store 4 numbers
+    let my_numbers = vec![10, 20, 30, 40];
+
+    // * Iterate through the vector using a for..in loop
+    //for num in my_numbers.iter() { // borrow
+    for num in &my_numbers {  // borrow
+        // * Determine whether to print the number or print "thirty" inside the loop
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{:?}", num),
+        } 
+    }
+    // * Use the .len() function to print the number of elements in a vector
+    println!("vector len: {:?}", my_numbers.len());
+}
