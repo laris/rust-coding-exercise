d12ebc7180609aed68ef64b3387b8d663edd9805
Laris Qiao <laris.qiao@gmail.com>
2023-10-15 17:08:18 +0800

    add a22 testing

--- rust-course-data/exercises/activities/src/bin/a22-11-4-testing-activity.rs
+++ rust-course-data/exercises/activities/src/bin/a22-11-4-testing-activity.rs
@@ -23,7 +23,10 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
-    Some(a / b)
+    match b {
+        0 => None,
+        _ => Some(a / b),
+    }
 }
 
 /// Takes two strings and places them immediately one after another.
@@ -32,3 +35,55 @@ fn concat(first: &str, second: &str) -> String {
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn check_clamp_lower() {
+        let input = (10, 100, 1000);
+        let result = clamp(input.0, input.1, input.2);
+        let expected = 100;
+        assert_eq!(result, expected, "n={}, lower={}, upper={}, error parse", input.0, input.1, input.2);
+    }
+    #[test]
+    fn check_clamp_upper() {
+        let input = (5000, 100, 1000);
+        let result = clamp(input.0, input.1, input.2);
+        let expected = 1000;
+        assert_eq!(result, expected, "n={}, lower={}, upper={}, error parse", input.0, input.1, input.2);
+    }
+    #[test]
+    fn check_clamp_mid() {
+        let input = (1, 0, 255);
+        let result = clamp(input.0, input.1, input.2);
+        let expected = 1;
+        assert_eq!(result, expected, "n={}, lower={}, upper={}, error parse", input.0, input.1, input.2);
+    }
+    #[test]
+    fn check_div_50_1() {
+        let result = div(50, 10);
+        let expected = Some(5);
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn check_div_50_zero() {
+        let result = div(50, 0);
+        let expected = None;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn check_concat_str() {
+        let result = concat("hello", "world!");
+        let expected = "hello world!".to_string();
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn check_concat_number_str() {
+        let result = concat("123", "4567".to_owned().as_str());
+        let expected = "123 4567".to_string();
+        assert_eq!(result, expected);
+    }
+}
