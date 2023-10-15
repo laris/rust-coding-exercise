690e09b8f8e1382db67ba31aa194767fdf170384
Laris Qiao <laris.qiao@gmail.com>
2023-10-15 18:00:09 +0800

    add a26 external crates

---
+++ rust-course-data/exercises/activities/src/bin/a26-11-5-external-crates.rs
@@ -0,0 +1,7 @@
+use humantime::format_duration;
+use std::time::Duration;
+
+fn main() {
+    let d = Duration::from_secs(9876);
+    println!("{}", format_duration(d));
+}
--- rust-course-data/exercises/activities/src/bin/a26-11-6-external-crates-activity.rs
+++ rust-course-data/exercises/activities/src/bin/a26-11-6-external-crates-activity.rs
@@ -8,4 +8,11 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::prelude::*;
+
+fn main() {
+    let local: DateTime<Local> = Local::now();
+    //println!("{:?}", local);
+    //println!("{:?}", local.format("%Y-%m-%d %H:%M:%S").to_string());
+    println!("{:?}", local.format("%Y.%m.%d-%H.%M.%S").to_string());
+}
