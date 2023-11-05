468db128ca1760eac008bed3cc3a5a89db8428b1
Laris Qiao <laris.qiao@gmail.com>
2023-11-05 17:11:49 +0800

    implement full functions with Vec, no hashmap

--- rust-course-data/exercises/activities/src/bin/p01-12-3-mini-program-intro.rs
+++ rust-course-data/exercises/activities/src/bin/p01-12-3-mini-program-intro.rs
@@ -34,16 +34,71 @@ struct Bill {
     amount: f64,
 }
 
+impl Bill {
+    fn new(name: String, amount: f64) -> Self {
+        Self { name, amount }
+    }
+    fn interactive_get_bill_from_input() -> Result<Bill, Box<dyn std::error::Error>> {
+        println!("Please input bill's name:");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        let name: String = input.trim().into();
+        input.clear();
+        println!("Please input bill's amount:");
+        std::io::stdin().read_line(&mut input)?;
+        let amount: f64 = input.trim().parse()?;
+        Ok(Self::new(name, amount))
+    }
+}
+
 fn view_bills(bills: &Vec<Bill>) {
     for (idx, bill) in bills.iter().enumerate() {
         println!("{idx}: name: {} amount: {}", bill.name, bill.amount);
     }
 }
 
+fn add_bills(bills: &mut Vec<Bill>) {
+    let bill = Bill::interactive_get_bill_from_input().unwrap();
+    bills.push(bill);
+}
+
+fn remove_bill(bills: &mut Vec<Bill>) -> Result<(), Box<dyn std::error::Error>> {
+    view_bills(bills);
+    println!("Which bill do you want to remove? Input leading idx number:");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    let idx: usize = input.trim().parse()?;
+    bills.remove(idx);
+    view_bills(bills);
+    Ok(())
+}
+
+fn edit_bill(bills: &mut Vec<Bill>) -> Result<(), Box<dyn std::error::Error>>{
+    view_bills(bills);
+    println!("Which bill do you want to edit? Input leading idx number:");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?; // read new input as String
+    let idx: usize = input.trim().parse()?; // parse String to usize
+    let new_bill = Bill::interactive_get_bill_from_input().unwrap(); // get new bill
+    let old_bill = bills.remove(idx);
+    println!("Are you confirm to replace \nthe old bill {old_bill:?}\nwith new bill {new_bill:?}? Yy/No?");
+    input.clear();
+    std::io::stdin().read_line(&mut input)?;
+    let confirm = input.trim().to_lowercase();
+    match confirm.as_str() {
+        "y" | "yes" | "true" => { bills.insert(idx, new_bill); view_bills(bills); },
+        "n" | "no" | "false" => { bills.insert(idx, old_bill); view_bills(bills); },
+        _ => (),
+    }
+    Ok(())
+}
+
 #[derive(Debug)]
 enum Cmd {
     View,
     Add,
+    Remove,
+    Edit,
     Quit,
     InvalidCmd(String),
 }
@@ -52,6 +107,8 @@ impl From<&str> for Cmd {
         match cmd {
             "1" | "view" => Cmd::View,
             "2" | "add" => Cmd::Add,
+            "3" | "remove" => Cmd::Remove,
+            "4" | "edit" => Cmd::Edit,
             "0" | "q" | "quit" | "exit" => Cmd::Quit,
             cmd@_ => Cmd::InvalidCmd(cmd.into()),
         }
@@ -64,14 +121,16 @@ fn parse_input() -> std::io::Result<Cmd> {
 }
 
 fn print_menu() {
-    println!("Menu:");
+    println!("----------Menu----------");
     println!("1. View bills");
     println!("2. Add bills");
+    println!("3. Remove bills");
+    println!("4. Edit bills");
     println!("0. Quit");
     println!("Please input the command:");
 }
 fn main() {
-    let mut bills = Vec::<Bill>::new();
+    let mut bills = vec![Bill { name: "Coffee".into(), amount: 9.99 }];
     loop {
         print_menu();
         match parse_input() {
@@ -79,6 +138,9 @@ fn main() {
             Ok(cmd) => {
                 match cmd {
                     Cmd::View => view_bills(&bills),
+                    Cmd::Add  => add_bills(&mut bills),
+                    Cmd::Remove => remove_bill(&mut bills).expect("todo for Ok(())"),
+                    Cmd::Edit => edit_bill(&mut bills).expect("todo for Ok(())"),
                     Cmd::Quit => break,
                     e@_ => { println!("Invalid cmd: {e:?}, please retry"); continue; }
                 }
