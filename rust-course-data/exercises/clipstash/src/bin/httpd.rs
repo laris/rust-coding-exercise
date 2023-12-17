7a4e9f5037a3c6816adfcacb00c7c12b8a4e6877
Laris Qiao <laris.qiao@gmail.com>
2023-12-17 12:02:42 +0800

    httpd p8 24-33 runing the server

---
+++ rust-course-data/exercises/clipstash/src/bin/httpd.rs
@@ -0,0 +1,37 @@
+use clipstash::data::AppDatabase;
+use clipstash::web::{renderer::Renderer};
+use dotenv::dotenv;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use clipstash::web::PageError::Render;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "httpd")]
+struct Opt {
+    #[structopt(default_value = "sqlite::data.db")]
+    connection_string: String,
+    #[structopt(short, long, parse(from_os_str), default_value="template/")]
+    template_directory: PathBuf,
+}
+
+fn main() {
+    dotenv().ok();
+    let opt = Opt::from_args();
+
+    let rt = tokio::runtime::Runtime::new()
+        .expect("failed to spawn tokio runtime");
+    let handle = rt.handle().clone();
+    rt.block_on(async move {
+        let renderer = Renderer::new(opt.template_directory);
+        let database = AppDatabase::new(&opt.connection_string).await;
+        let config = clipstash::RocketConfig {
+            renderer,
+            database,
+        };
+
+        clipstash::rocket(config)
+            .launch()
+            .await
+            .expect("failed to launch rocket server")
+    });
+}
